dc6fc0764d32642c1531781bbc1d32e9223e8d92
Miguel Ángel García <magarcial.dev@outlook.es>
2024-04-27 15:11:46 +0200

    Refactorización lógica y estilos css

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+const hands = ["rock", "paper", "scissors"];
+const winners = {
+    "rock" : "scissors",
+    "paper" : "rock",
+    "scissors" : "paper"
+};
+
 const paperButton = document.querySelector('#paper');
 const rockButton = document.querySelector('#rock');
 const scissorsButton = document.querySelector('#scissors');
-const score = document.querySelector('#score');
-const playAgainButton = document.querySelector('#playAgain');
-let userHand, cpuHand, vecesJugado = 0, colorBotonPlayAgain;
 
+const score = document.querySelector('#score');
+const resultContainer = document.querySelector('.result-container');
+const handsContainer = document.querySelector('.game-container');
+
+let gameHands, colorBotonPlayAgain;
+const playAgainButton = document.querySelector('#playAgain');
 const rulesButton = document.querySelector('.rules');
 const modalWindow = document.querySelector('#ventanaModal');
 const closeButton = document.querySelector('#close-modal');
-
-score.innerHTML = '<span>0</span>';
 
 /**
  * Ventana modal
  */
 rulesButton.addEventListener('click', () => {
+    modalWindow.removeAttribute('style');
     modalWindow.classList.add('flex');
 });
 
@@ -24,8 +33,9 @@ closeButton.addEventListener('click', () => {
 });
 
 // Si el usuario hace click fuera de la ventana, se cierra.
-window.addEventListener('click',function(event) {
-    if (event.target === modalWindow) {
+window.addEventListener('click', function(event) {
+    if ( event.target === modalWindow ) {
+        modalWindow.style.display = "none";
         modalWindow.classList.remove('flex');
     }
 });
@@ -33,101 +43,105 @@ window.addEventListener('click',function(event) {
 /**
  * LÓGICA DE JUEGO
  */
-[paperButton, rockButton, scissorsButton].forEach( btn => {
+
+[ paperButton, rockButton, scissorsButton ].forEach( btn => {
     btn.addEventListener('click', () => {
-        userHand = btn.id, 
-        cpuHand = generateRandomHand(),
-        vecesJugado += 1;
-        selectWinner(userHand, cpuHand);
+        gameHands = selectHand( btn );
+        const result = selectWinner( gameHands );
+
+        cambiarTablero( 'resultado' );
+        setTableroGameHands( gameHands );
+        setTableroStyles( result, gameHands );
     });
 });
 
 playAgainButton.addEventListener('click', () => {
+    const userHand = gameHands[0], cpuHand = gameHands[1];
     document.querySelector('#userHand').classList.remove(`${userHand}`);
     document.querySelector('#cpuHand').classList.remove(`${cpuHand}`);
-    document.querySelector('.playAgainText').classList.remove(`${colorBotonPlayAgain}TextColor`);
-    cambiarTablero('seleccion');
+    cambiarTablero( 'seleccion' );
 });
-
-const hands = ["rock", "paper", "scissors"];
-const winners = {
-    "rock" : "scissors",
-    "paper" : "rock",
-    "scissors" : "paper"
-};
-
-function selectWinner(userHand, cpuHand) {
-    const resultText = document.querySelector('.result-text');
-    const playAgainText = document.querySelector('.playAgainText');
-
-    if ( userHand === cpuHand ) {
-        colorBotonPlayAgain = 'win';
-        resultText.innerText = "DRAW";
-        playAgainText.classList.add('winTextColor');
-    }
-    
-    if ( winners[userHand] === cpuHand ) {
-        cambiarScore('w');
-        colorBotonPlayAgain = 'win';
-        resultText.innerText = 'YOU WIN';
-        playAgainText.classList.add('winTextColor');
-    }
-
-    if ( winners[cpuHand] === userHand ) {
-        cambiarScore('l');
-        colorBotonPlayAgain = 'lose';
-        resultText.innerText = 'YOU LOSE';
-        playAgainText.classList.add('loseTextColor');
-    }
-
-    cambiarTablero('juego', userHand, cpuHand);
-}
 
 function generateRandomHand() {
     return hands[Math.floor(Math.random()*3)];
 }
 
-function cambiarScore(resultado) {
+function selectHand( btnHand ) {
+    return [ btnHand.id, generateRandomHand() ];
+}
+
+function selectWinner( gameHands ) {
+    const userHand = gameHands[0], cpuHand = gameHands[1];
+    let result;
+
+    if ( userHand === cpuHand ) {
+        result = 'D';
+    }
+    
+    if ( winners[userHand] === cpuHand ) {
+        result = 'W';
+        cambiarScore('W');
+    }
+
+    if ( winners[cpuHand] === userHand ) {
+        result = 'L';
+        cambiarScore('L');
+    }
+
+    return result;
+}
+
+function cambiarScore( resultado ) {
     const anteriorScore = parseInt(score.innerText);
-    if ( resultado === 'w' ) {
+    if ( resultado === 'W' ) {
         score.innerText = anteriorScore + 1;
     }
-    if ( resultado === 'l' && anteriorScore === 0 ) {
+    if ( resultado === 'L' && anteriorScore === 0 ) {
         score.innerText = anteriorScore;
     }
-    if ( resultado === 'l' && anteriorScore !== 0 ) {
+    if ( resultado === 'L' && anteriorScore !== 0 ) {
         score.innerText = anteriorScore - 1;
     }
 }
 
-function cambiarTablero(modo, userHand, cpuHand) {
-    let tableroJuego = document.querySelector('.result-box'),
-    tableroSeleccion = document.querySelector('.game-box');
-
-    if ( modo === 'juego' ) {
-        document.querySelector('.game-container').style.background = 'none';
-        cambiarSeleccion(userHand, cpuHand);
-        tableroJuego.style.display = 'flex';
-        tableroSeleccion.style.display = 'none';
+function cambiarTablero( modo ) {        
+    if ( modo === 'resultado' ) {
+        handsContainer.style.display = 'none';
+        resultContainer.style.display = 'flex';
     }
 
     if ( modo === 'seleccion' ) {
-        document.querySelector('.game-container').style.background = 'no-repeat center url("./img/bg-triangle.svg")';
-        tableroJuego.style.display = 'none';
-        tableroSeleccion.style.display = 'block';
+        resultContainer.style.display = 'none';
+        handsContainer.style.display = 'block';
     }
-
-    document.querySelector('.result-text').classList.add('tracking-in-expand-fwd');
 }
 
-function cambiarSeleccion(userHand, cpuHand) {
-    let userBox = document.querySelector('#userHand'),
-    userImg = document.querySelector('#userHand img'),
-    cpuBox = document.querySelector('#cpuHand'),
-    cpuImg = document.querySelector('#cpuHand img');
+function setTableroGameHands( manos ) {
+    const userHand = manos[0], cpuHand = manos[1];
+    const userHandContainer = document.querySelector('#userHand'),
+    cpuHandContainer = document.querySelector('#cpuHand');
 
-    userBox.classList.add(userHand);
-    cpuBox.classList.add(cpuHand);
-    userImg.src = `img/icon-${userHand}.svg`;
-    cpuImg.src = `img/icon-${cpuHand}.svg`;
+    /** Se agregan los botones con la selección de cada jugador */
+    userHandContainer.innerHTML = `<img src="img/icon-${userHand}.svg" alt="${userHand}"/>`;
+    cpuHandContainer.innerHTML = `<img src="img/icon-${cpuHand}.svg" alt="${cpuHand}"/>`;
+}
+
+function setTableroStyles( resultado, manos ) {
+    const userHand = manos[0], cpuHand = manos[1];
+    const colorBotonPlayAgain = resultado === 'W' ? 'win' : 'lose';
+    document.querySelector('.playAgainText').classList.add(`${colorBotonPlayAgain}TextColor`);
+
+    document.querySelector('#userHand').classList.add(`${userHand}`);
+    document.querySelector('#cpuHand').classList.add(`${cpuHand}`);
+    switch ( resultado ) {
+        case 'W':
+            document.querySelector('.result-text').innerText = 'YOU WIN';
+            break;
+        case 'L':
+            document.querySelector('.result-text').innerText = 'YOU LOSE';
+            break;
+        case 'D':
+            document.querySelector('.result-text').innerText = 'IT\'S A DRAW';
+            break;
+    }
 }
